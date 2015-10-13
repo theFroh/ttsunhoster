965f55cc56dd6f76abcef91bf33a74555d238e78
Luke <the_froh@hotmail.com>
2015-10-13 11:34:17 +0800

    Moved to Requests

--- unhoster.py
+++ unhoster.py
@@ -2,10 +2,14 @@ import os
 from json import load
 import pprint
 from enum import Enum
+
+import requests
 import urllib.parse
 
 import concurrent.futures
 import urllib.request
+
+MAX_WORKERS = 15
 
 class DataType(Enum):
     image = 1
@@ -19,9 +23,13 @@ MESH_DATA = {
     "ColliderURL": DataType.model
     }
 
-def load_tts_url(tts_url, timeout):
-    with urllib.request.urlopen(tts_url[0], timeout=timeout) as conn:
-        return conn.read()
+def load_tts_url(tts_url):
+    r = requests.get(tts_url[0])
+
+    if r.status_code == requests.codes.ok:
+        return r.content
+    else:
+        print("Error", tts_url[0], r.status_code)
 
 def url_to_tts(url):
     url_path = urllib.parse.urlparse(url).path
@@ -29,20 +37,97 @@ def url_to_tts(url):
 
     return "".join([c for c in url if c.isalpha() or c.isdigit()]).rstrip() + url_ext
 
-if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser(description="Unhosts Tabletop Simulator workshop custom content.")
-    parser.add_argument("json_input", help="path to the .json file")
-    args = parser.parse_args()
-
-    with open(args.json_input, "r") as fp:
+def parse_tts_custom_object(workshop_json):
+    with open(workshop_json, "r") as fp:
         save = load(fp)
         objects = save["ObjectStates"]
-        save_name = save["SaveName"]
 
-    output_dir = os.path.join(os.path.dirname(os.path.abspath(args.json_input)),
-        "".join([c for c in save_name if c.isalpha() or c.isdigit() or c == ' ']).rstrip())
+    image_urls = {}
+    model_urls = {}
+
+    for obj in objects:
+        if "CustomMesh" in obj:
+            custom_mesh = obj["CustomMesh"]
+            for key,dtype in MESH_DATA.items():
+                if key in custom_mesh:
+                    url = custom_mesh[key]
+                    if url: # make sure not empty
+                        if url[:4] != "http":
+                            url = "http://" + url # partly handles these
+                        if dtype == DataType.image:
+                            image_urls[(url, dtype)] = url_to_tts(url)
+                        elif dtype == DataType.model:
+                            model_urls[(url, dtype)] = url_to_tts(url)
+
+    return image_urls, model_urls
+
+if __name__ == '__main__':
+    download = False
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(description="Unhosts Tabletop Simulator workshop custom content.")
+    parser.add_argument("json_input", help="path to either a WorkshopFileInfos file, or one or more Workshop mod .json files", nargs="+")
+    parser.add_argument("--output", "-o", help="where to store the Models and Images subdirectories")
+    args = parser.parse_args()
+
+    all_image_urls = {}
+    all_model_urls = {}
+
+    if len(args.json_input) == 1 and os.path.basename(args.json_input[0]) == "WorkshopFileInfos.json": # this wont ever break, ever! please
+        print("Loaded WorkshopFileInfos.json; listing json names")
+
+        with open(args.json_input[0], "r") as fp:
+            file_infos = load(fp)
+
+        name_map = {}
+        for info in file_infos:
+            directory = info["Directory"]
+            # print(info, directory[-4:])
+
+            if directory[-4:] == "json": # I mean, we are only looking for json.
+                name_map[info["Name"]] = directory
+
+        pprint.pprint(name_map)
+
+        print("\nType the name, or start of the name of the workshop mod you wish to dump.")
+
+        name = None
+        while True:
+            query = input("> ").lower()
+            matches = list(n for n in list(name_map) if n.lower().startswith(query))
+
+            if query == "q" or query == "quit":
+                sys.exit()
+
+            if matches and len(matches) == 1: # try to be specific.
+                name = matches[0]
+                break
+            elif len(matches) > 1:
+                print("Multiple matches, be more specific;",matches)
+            else:
+                print("No matches found.")
+
+        print(name)
+        image_urls, model_urls = parse_tts_custom_object(name_map[name])
+
+        all_image_urls.update(image_urls)
+        all_model_urls.update(model_urls)
+
+    else:
+        for json in args.json_input:
+            print(json)
+            image_urls, model_urls = parse_tts_custom_object(json)
+
+            all_image_urls.update(image_urls)
+            all_model_urls.update(model_urls)
+
+    if args.output:
+        output_dir = args.output
+    else:
+        output_dir = os.path.join(os.path.dirname(os.path.abspath(args.json_input[0])),
+            "Retrieved")
+
     print("Output directory:", output_dir)
 
     if not os.path.exists(output_dir):
@@ -55,55 +140,38 @@ if __name__ == '__main__':
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
 
-
-    image_urls = {}
-    model_urls = {}
-
-    for obj in objects:
-        # grab a usable name
-        name = obj["GUID"] # default to what always exists
-        if "Nickname" in obj:
-            name = obj["Nickname"]
-        print("Object", name)
-
-        if "CustomMesh" in obj:
-            custom_mesh = obj["CustomMesh"]
-            for key,dtype in MESH_DATA.items():
-                if key in custom_mesh:
-                    url = custom_mesh[key]
-                    if url: # make sure not empty
-                        if dtype == DataType.image:
-                            print("\tIMAGE", key, url)
-                            image_urls[(url, dtype)] = url_to_tts(url)
-                        elif dtype == DataType.model:
-                            print("\tMODEL", key, url)
-                            model_urls[(url, dtype)] = url_to_tts(url)
-
     print("\nURL's to nab:")
 
     # get a list of all urls
-    all_urls = list(model_urls)
-    all_urls.extend(list(image_urls))
+    all_urls = list(all_model_urls)
+    all_urls.extend(list(all_image_urls))
 
     pprint.pprint(all_urls)
 
-    print("\nGrabbing files")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
-        future_to_url = {executor.submit(load_tts_url, url, 60): url for url in all_urls}
+    print("\nGrabbing {} files".format(len(all_urls)))
+    n = 0
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        future_to_url = {executor.submit(load_tts_url, url): url for url in all_urls}
         for future in concurrent.futures.as_completed(future_to_url):
             url = future_to_url[future]
-            try:
-                data = future.result()
-            except Exception as exc:
-                print('%r generated an exception: %s' % (url, exc))
-            else:
-                print('%r is %d bytes' % (url[0], len(data)), end="")
+            data = future.result()
 
-                if url[1] == DataType.image and url in image_urls:
-                    with open(os.path.join(image_dir, image_urls[url]), "wb") as fp:
-                        fp.write(data)
-                elif url[1] == DataType.model and url in model_urls:
-                    with open(os.path.join(model_dir, model_urls[url]), "wb") as fp:
-                        fp.write(data)
+            print("(%d/%d) %s" % (n, len(all_urls), url[0]))
 
-                print("... Saved.")
+            if data:
+                try:
+                    if url[1] == DataType.image:
+                        with open(os.path.join(image_dir, all_image_urls[url]), "wb") as fp:
+                            fp.write(data)
+                    elif url[1] == DataType.model:
+                        with open(os.path.join(model_dir, all_model_urls[url]), "wb") as fp:
+                            fp.write(data)
+                except Exception as e:
+                    print("Exception while writing", url)
+                    print(e)
+
+                print("\t... Saved.")
+                sys.stdout.flush()
+            n += 1
+
+    print("Done!")
